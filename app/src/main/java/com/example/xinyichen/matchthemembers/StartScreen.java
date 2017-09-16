7c98b8a129ec472ce16cc5543e8a50a3f1551e9a
Xin Yi Chen <xinyichen516@gmail.com>
2017-09-15 20:24:09 -0700

    Added feature to open link to MDB site

--- app/src/main/java/com/example/xinyichen/matchthemembers/StartScreen.java
+++ app/src/main/java/com/example/xinyichen/matchthemembers/StartScreen.java
@@ -1,10 +1,12 @@
 package com.example.xinyichen.matchthemembers;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -22,6 +24,17 @@ public class StartScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageView logo = (ImageView)findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.mobiledevsberkeley.org/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
